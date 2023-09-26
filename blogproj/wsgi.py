af3c12d455054a171db7d87d8bd7ce19d83207ff
Pierce O'Neill <pierceoneillky@gmail.com>
2023-09-26 13:58:27 +0100

    initial commit

--- blogproj/wsgi.py
+++ blogproj/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogproj.blogproj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogproj.settings')
 
 application = get_wsgi_application()
