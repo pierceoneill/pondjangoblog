ab7197b3ed51b1eae99b6e42b67af23840b871c6
Pierce O'Neill <pierceoneillky@gmail.com>
2023-09-26 18:06:55 +0100

    modification of header and footer

--- users/models.py
+++ users/models.py
@@ -1,21 +1,18 @@
 from django.db import models
-from datetime import datetime
+from django.contrib.auth.models import User
 from PIL import Image
 
-class User(models.Model):
-  name = models.CharField(max_length=200)
-  email = models.CharField(max_length=100)
-  image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-  def __str__(self):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
         return f'{self.user.username} Profile'
 
-  def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-  user_id = models.IntegerField(blank=True)
-  def __str__(self):
-    return self.name
