20900b84084ecdb589dd0054306cbb135185a153
Pierce O'Neill <pierceoneillky@gmail.com>
2023-09-26 18:01:23 +0100

    modification of header and footer

--- users/views.py
+++ users/views.py
@@ -1,71 +1,47 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages, auth
-from django.contrib.auth.models import User
-from users.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
+from django.contrib.auth.decorators import login_required
+
 
 def register(request):
-  if request.method == 'POST':
-    # Get form values
-    first_name = request.POST['first_name']
-    last_name = request.POST['last_name']
-    username = request.POST['username']
-    email = request.POST['email']
-    password = request.POST['password']
-    password2 = request.POST['password2']
-
-    # Check if passwords match
-    if password == password2:
-      # Check username
-      if User.objects.filter(username=username).exists():
-        messages.error(request, 'That username is taken')
-        return redirect('register')
-      else:
-        if User.objects.filter(email=email).exists():
-          messages.error(request, 'That email is being used')
-          return redirect('register')
-        else:
-          # Looks good
-          user = User.objects.create_user(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
-          # Login after register
-          # auth.login(request, user)
-          # messages.success(request, 'You are now logged in')
-          # return redirect('index')
-          user.save()
-          messages.success(request, 'You are now registered and can log in')
-          return redirect('login')
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Successfully Created for {username} Login In Now!!!')
+            return redirect('login')
     else:
-      messages.error(request, 'Passwords do not match')
-      return redirect('register')
-  else:
-    return render(request, 'accounts/register.html')
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
-def login(request):
-  if request.method == 'POST':
-    username = request.POST['username']
-    password = request.POST['password']
 
-    user = auth.authenticate(username=username, password=password)
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
-    if user is not None:
-      auth.login(request, user)
-      messages.success(request, 'You are now logged in')
-      return redirect('dashboard')
+@login_required
+def profile_update(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
     else:
-      messages.error(request, 'Invalid credentials')
-      return redirect('login')
-  else:
-    return render(request, 'accounts/login.html')
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
-def logout(request):
-  if request.method == 'POST':
-    auth.logout(request)
-    messages.success(request, 'You are now logged out')
-    return redirect('index')
-
-def dashboard(request):
-  user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
-
-  context = {
-    'contacts': user_contacts
-  }
-  return render(request, 'accounts/dashboard.html', context)
+    context = {
+        "u_form": u_form,
+        "p_form": p_form
+    }
+    return render(request, 'users/profile_update.html', context)
