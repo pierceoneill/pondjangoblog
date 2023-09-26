8612f82d7b2f1c617202abaadff304729f5bc4f3
Pierce O'Neill <pierceoneillky@gmail.com>
2023-09-26 18:26:44 +0100

    modification of header and footer

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.files.storage import default_storage as storage
 from .models import Profile
 
 
